81d373f466ff51a88a9f86bfa1f3408d3c49cb5a
NickBash <nikitabash7@gmail.com>
2020-11-09 18:43:49 +0500

    initial

---
+++ client/src/pages/AuthPage.js
@@ -0,0 +1,75 @@
+import React, {useState, useEffect, useContext} from 'react'
+import {useHttp} from "../hooks/http.hook";
+import {useMessage} from "../hooks/message.hook";
+import {AuthContext} from "../context/AuthContext";
+
+export const AuthPage = () => {
+    const auth = useContext(AuthContext)
+    const message = useMessage()
+    const {loading, error, request, clearError} = useHttp()
+    const [form, setForm] = useState({
+        email: '', password: ''
+    })
+
+    useEffect(() => {
+        message(error)
+        clearError()
+    }, [error, message, clearError])
+
+    useEffect( () => {
+        window.M.updateTextFields()
+    }, [])
+
+    const changeHandler = event => {
+        setForm({...form, [event.target.name]: event.target.value})
+    }
+
+    const registerHandler = async () => {
+        try {
+            const data = await request('/api/auth/register', 'POST', {...form})
+           message(data.message)
+        } catch (e) {
+
+        }
+    }
+
+    const loginHandler = async () => {
+        try {
+            const data = await request('/api/auth/login', 'POST', {...form})
+            auth.login(data.token, data.userId)
+        } catch (e) {
+
+        }
+    }
+
+    return (
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <h2 className="center">Сервис сокращения ссылок</h2>
+                <div className="card grey lighten-5 z-depth-1">
+                    <div className="card-content">
+                        <span className="card-title center-align">Авторизация</span>
+                        <div>
+
+                            <div className="input-field">
+                                <input id="email" type="text" name="email" value={form.email}
+                                       className="validate" placeholder="Введите email" onChange={changeHandler}/>
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div className="input-field">
+                                <input id="password" type="password" name="password" value={form.password}
+                                       className="validate" placeholder="Введите password" onChange={changeHandler}/>
+                                <label htmlFor="password">Password</label>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="card-action">
+                        <button className="btn yellow darken-4" onClick={loginHandler} disabled={loading}>Войти</button>
+                        <button className="btn blue-4 reg-btn" onClick={registerHandler} disabled={loading}>Регистрация</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
